a96356ad8d4693856554a101eabdfa1beea5f9e6
nathanbuyr <G00421082@atu.ie>
2024-10-25 13:35:11 +0000

    Ex4) Route accepts name and surname parameter

--- server.js
+++ server.js
@@ -14,3 +14,14 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
 });
+
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+
+app.get('/hello/:name/:surname', (req, res) => {
+    const name = req.params.name;
+    const surname = req.params.surname;
+    res.send(`Hello ${name} + ${surname}`);
+});
