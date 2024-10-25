4ded627c33973920af4ba397b114f4c0834b54cf
nathanbuyr <G00421082@atu.ie>
2024-10-25 13:56:06 +0000

    Ex5) Json data accepted

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, next) => { //Error handling to catch any server errors
     console.error(err.stack);
-    res.status(500).send('Something went wrong!');
+    res.status(500).send('Something went wrong!'); 
 });
 
-app.get('/hello/:name', (req, res) => {
+app.get('/hello/:name', (req, res) => { //Routing with a URL parameter
     const name = req.params.name;
     res.send(`Hello ${name}`);
 });
@@ -24,4 +24,31 @@ app.get('/hello/:name/:surname', (req, res) => {
     const name = req.params.name;
     const surname = req.params.surname;
     res.send(`Hello ${name} + ${surname}`);
+});
+
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    res.status(200).json({ myMovies:movies }); //Modifying the route handler to return the movies as a JSON response
 });
